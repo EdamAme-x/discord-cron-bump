2222184bd204861fb4e31b4b459eb17df18a01e9
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-20 05:03:39 +0000

    add

--- index.ts
+++ index.ts
@@ -7,6 +7,28 @@ console.log(`[@] ${channelId} : by ${auth}`);
 const app = new Hono();
 
 app.get("/", (c) => c.text("CRON SERVER"));
+fetch("https://discord.com/api/v9/channels/" + channelId + "/messages", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "ja,en-US;q=0.9,en;q=0.8,ur;q=0.7,ig;q=0.6,ti;q=0.5,zh-CN;q=0.4,zh;q=0.3,sl;q=0.2,it;q=0.1",
+    "authorization": auth,
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Chrome OS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-debug-options": "bugReporterEnabled",
+    "x-discord-locale": "ja",
+    "x-discord-timezone": "Asia/Tokyo",
+  },
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"mobile_network_type\":\"wifi\",\"content\":\"" + "定期話題盛り上げBOTが起動しました！ TIME:" + Date.now() + "\", \"tts\":false,\"flags\":0}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
 
 Deno.cron("WADAI1", "0 */6 * * *", () => {
   const content = Math.random() > 0.5 ? "今皆何してる？" : "ねむいかもー"
