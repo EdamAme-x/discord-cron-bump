acdffcb1cd94a677f29c29e4e5e5547a5f4938f9
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-20 05:17:11 +0000

    add

--- index.ts
+++ index.ts
@@ -55,7 +55,7 @@ Deno.cron("WADAI1", "0 */6 * * *", () => {
   });
 })
 
-Deno.cron("YORU1", "22 * * * *", () => {
+Deno.cron("YORU1", "0 22 * * *", () => {
   const content = Math.random() > 0.45 ? "そろそろ寝るわおやすみ" : "全然寝れない おやすみ"
   fetch("https://discord.com/api/v9/channels/" + channelId + "/messages", {
     "headers": {
@@ -81,7 +81,7 @@ Deno.cron("YORU1", "22 * * * *", () => {
   });
 })
 
-Deno.cron("ASA1", "6 * * * *", () => {
+Deno.cron("ASA1", "0 6 * * *", () => {
   const content = Math.random() > 0.5 ? "おはよー" : "おはよ♡"
   fetch("https://discord.com/api/v9/channels/" + channelId + "/messages", {
     "headers": {
@@ -106,5 +106,60 @@ Deno.cron("ASA1", "6 * * * *", () => {
     "credentials": "include"
   });
 })
+
+Deno.cron("NEWS1", "0 */2 * * *", () => {
+  const content = `
+${Math.random() > 0.5 ? "これおもろ" : "これえぐくね"}
+${getNews()}
+`
+  fetch("https://discord.com/api/v9/channels/" + channelId + "/messages", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "ja,en-US;q=0.9,en;q=0.8,ur;q=0.7,ig;q=0.6,ti;q=0.5,zh-CN;q=0.4,zh;q=0.3,sl;q=0.2,it;q=0.1",
+      "authorization": auth,
+      "content-type": "application/json",
+      "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Chrome OS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-debug-options": "bugReporterEnabled",
+      "x-discord-locale": "ja",
+      "x-discord-timezone": "Asia/Tokyo",
+    },
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "{\"mobile_network_type\":\"wifi\",\"content\":\"" + content + "\", \"tts\":false,\"flags\":0}",
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  });
+})
+
+async function getNews() {
+  const resp = fetch("https://newsapi.org/v2/top-headlines?country=jp&apiKey=4c045d9d62fe410c973a561218e41683", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "ja,en-US;q=0.9,en;q=0.8,ur;q=0.7,ig;q=0.6,ti;q=0.5,zh-CN;q=0.4,zh;q=0.3,sl;q=0.2,it;q=0.1",
+      "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Chrome OS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-requested-with": "XMLHttpRequest"
+    },
+    "referrer": "https://newsapi.org/s/japan-news-api",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  })
+
+  const data = resp.json();
+
+  return data["articles"][Math.floor(data["articles"].length * Math.random())]["url"];
+}
 
 Deno.serve(app.fetch)
